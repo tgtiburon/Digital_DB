63558088fcd74e4b384ed45cf482da5705510448
tgtiburon <tg.tiburon@gmail.com>
2021-12-31 14:18:56 -0500

    connected to DB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,40 @@
+// Hide credentials with dotenv
 require('dotenv').config();
-
+// import the Sequelize Constructor from the library
 const Sequelize = require('sequelize');
+let sequelize;
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+
+
+
+
+// TODO:  For when it is live using jawsdb
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+if(process.env.JAWSDB_URL) {
+  // If JAWSDB_URL is found then use this.
+
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+}else {
+ 
+  // If no JAWSDB_URL then use local for dev purposes
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    dialectOptions: {
+     decimalNumbers: true,
+    },
+
+  });
+}
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+//sync sequelize models to the database, then turn on the server
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+
+// turn on connection to db and server
+// force: false tells it to to not DROP tables
+// but if we set it true it will drop...good for changing table structures.
+
+sequelize.sync({ force: true }).then(() => {
+
+  app.listen(PORT, ()=> console.log(`Now listening on port: ${PORT}!`));
+
 });
